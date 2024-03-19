25db5db68562b80f307acdae96e9ef9f4a395622
Lidiia Dubovetc <dubovetslida@gmail.com>
2024-03-19 13:56:55 +0100

    Changes for Sprint 4

--- tests.py
+++ tests.py
@@ -20,7 +20,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
@@ -33,7 +33,7 @@ class TestBooksCollector:
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     def test_set_book_genre_book_in_books_genre_and_genre_in_genre(self):
 
@@ -61,52 +61,55 @@ class TestBooksCollector:
 
         assert collector.get_book_genre('После бала') == 'Детективы'
 
-    @pytest.mark.parametrize('new_book, book_genre', [['Анна Каренина', 'Ужасы'], ['После бала', 'Детективы'], ['Детство, отрочество, юность', 'Ужасы']])
-    def test_get_books_with_specific_genre_two_books_for_one_genre(self, new_book, book_genre):
-
+    @pytest.mark.parametrize('new_book, book_genre', [['Анна Каренина', 'Ужасы'], ['После бала', 'Ужасы'],
+                                                      ['Детство, отрочество, юность', 'Ужасы']])
+    def test_get_books_with_specific_genre_three_books_for_one_genre(self, new_book, book_genre):
         collector = BooksCollector()
 
         collector.add_new_book(new_book)
         collector.set_book_genre(new_book, book_genre)
 
-        assert collector.get_books_with_specific_genre('Ужасы') == ['Анна Каренина', 'Детство, отрочество, юность']
+        assert new_book in collector.get_books_with_specific_genre(book_genre)
 
     def test_get_books_genre_current(self):
         collector = BooksCollector()
+        collector.add_new_book('Анна Каренина')
+        collector.set_book_genre('Анна Каренина', 'Ужасы')
 
-        assert collector.get_books_genre() == ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+        assert collector.get_books_genre() == {'Анна Каренина': 'Ужасы'}
 
-    @pytest.mark.parametrize('new_book, book_genre', [['Гарри Поттер', 'Фантастика'], ['Хэллоуин', 'Ужасы'], ['Розовая пантера', 'Детективы'], ['Три поросенка', 'Мультфильмы'], ['Один дома', 'Комедии']])
-    def test_get_books_for_children_three_books_out_of_five_are_ok_for_children(self, new_book, book_genre):
-
+    @pytest.mark.parametrize('new_book, book_genre',
+                             [['Гарри Поттер', 'Фантастика'], ['Три поросенка', 'Мультфильмы'],
+                              ['Один дома', 'Комедии']])
+    def test_get_books_for_children_three_books_are_ok_for_children(self, new_book, book_genre):
         collector = BooksCollector()
 
         collector.add_new_book(new_book)
         collector.set_book_genre(new_book, book_genre)
 
-        assert collector.get_books_for_children() == ['Гарри Поттер', 'Три поросенка', 'Один дома']
+        assert new_book in collector.get_books_for_children()
+
 
     def test_add_book_in_favorites_one_book(self):
-
         collector = BooksCollector()
 
         collector.add_new_book('Три поросенка')
         collector.add_book_in_favorites('Три поросенка')
 
-        assert 'Три поросенка' in collector.favorites()
+        assert collector.get_list_of_favorites_books() == ['Три поросенка']
 
     def test_delete_book_from_favorites_one_book(self):
-
         collector = BooksCollector()
 
         collector.add_new_book('Три поросенка')
         collector.add_book_in_favorites('Три поросенка')
+        collector.add_new_book('Гарри Поттер')
+        collector.add_book_in_favorites('Гарри Поттер')
         collector.delete_book_from_favorites('Три поросенка')
 
-        assert 'Три поросенка' not in collector.favorites()
+        assert collector.get_list_of_favorites_books() == ['Гарри Поттер']
 
     def test_get_list_of_favorites_books_two_books_of_three_are_favorite(self):
-
         collector = BooksCollector()
 
         collector.add_new_book('Гарри Поттер')
@@ -115,4 +118,4 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Гарри Поттер')
         collector.add_book_in_favorites('Три поросенка')
 
-        assert collector.favorites() == ['Гарри Поттер', 'Три поросенка']
+        assert collector.get_list_of_favorites_books() == ['Гарри Поттер', 'Три поросенка']
